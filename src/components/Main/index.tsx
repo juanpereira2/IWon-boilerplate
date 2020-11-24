6f9e138d92257f667fcf91f3cb5cdb6b311dc3e8
juanpereira2 <juanpereira74@hotmail.com>
2020-11-24 16:57:49 -0300

    added styled componnents and somes svg

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,3 +1,12 @@
-const Main = () => <main><h1>react Avançado</h1></main>
+import * as S from './styles'
+
+
+const Main = () =>
+    <S.Wrapper>
+        <S.Logo src="/img/logo.svg" alt="imagem logo e texto do lado react avançado" />
+        <S.Title>react Avançado</S.Title>
+        <S.Description>React, Next, typescript and Styled Component </S.Description>
+        <S.Ilustration src="/img/hero-illustration.svg" alt="dev em frente ao pc" />
+    </S.Wrapper>
 
 export default Main
